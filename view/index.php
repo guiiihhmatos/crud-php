807c723e3636c30b8a47bc44ce0221c7a5e7e3e9
gkgames <gkgamesclans@gmail.com>
2023-03-03 14:28:47 -0300

    IMPROVEMENT - crud php

---
+++ view/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Login - gms</title>
+</head>
+<body>
+    
+    <div class="container">
+
+        <div class="container-left">
+            <h2>Loja virtual</h2>
+
+            <p style="margin-top: 80px; margin-bottom: 80px;">Faça login na nossa loja virtual de games</p>
+
+            <a href="#">Cadastre-se</a>
+        </div>
+
+        <div class="container-right">
+            
+            <div class="title">
+                <h2>Login</h2>
+            </div>
+
+            <form class="inputs" method="POST" action="aula01.php">
+                <input type="email" class="input__email" placeholder="Seu email ...">
+                <input type="password" class="input__password" placeholder="Sua senha ...">
+            </form>
+
+            <div class="button">
+                <button class="button__enviar">Enviar</button>
+            </div>
+            
+        </div>
+        
+    </div>
+
+
+</body>
+</html>
